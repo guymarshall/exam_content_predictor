a9971d916255b0998ab20f492568e5f9d9e86ad9
Guy Marshall <guymarshall9@gmail.com>
2023-04-07 21:46:15 +0100

    Move contents of subject.rs to main.rs and remove subject.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-mod subject;
 mod csv_reader;
+
+use std::collections::HashMap;
+
+struct Subject {
+    name: String,
+    specification: HashMap<String, String>
+}
+
+impl Subject {
+    fn new(subject_name: &str, subject_specification: HashMap<&str, &str>) -> Subject {
+        let specification: HashMap<String, String> = subject_specification
+            .iter()
+            .map(|(&key, &value)| (key.to_string(), value.to_string()))
+            .collect();
+
+        Subject {
+            name: subject_name.to_string(),
+            specification,
+        }
+    }
+}
 
 fn main() {
     match csv_reader::read("test.csv") {
