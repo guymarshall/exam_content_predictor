2938dd15ffe89d1e7a33b097bed731388a2735d3
Guy Marshall <guymarshall9@gmail.com>
2023-04-07 22:33:32 +0100

    Remove subject struct and impl

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,6 @@ impl Specification {
     }
 }
 
-struct Subject {
-    name: String,
-    specification: HashMap<String, String>
-}
-
-impl Subject {
-    fn new(subject_name: &str, subject_specification: HashMap<&str, &str>) -> Subject {
-        let specification: HashMap<String, String> = subject_specification
-            .iter()
-            .map(|(&key, &value)| (key.to_string(), value.to_string()))
-            .collect();
-
-        Subject {
-            name: subject_name.to_string(),
-            specification,
-        }
-    }
-}
-
 fn main() {
     match csv_reader::read("test.csv") {
         Ok(map) => println!("{:?}", map),
